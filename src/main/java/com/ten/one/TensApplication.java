f0c296e168dff3f9f13fcc0d46adedd18c43ab76
Ilankeer <ilankeer619@gmail.com>
2023-08-19 11:19:08 +0530

    first file

---
+++ src/main/java/com/ten/one/TensApplication.java
@@ -0,0 +1,13 @@
+package com.ten.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ten/one/TensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ten.one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
